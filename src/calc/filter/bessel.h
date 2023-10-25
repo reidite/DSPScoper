95a7814af63b5b9cfe57fc1091973170639ff6be
Nguyễn Lê Quốc Việt <nglequocviet@gmail.com>
2023-10-25 17:07:24 +0700

    [a] starting adding bessel filter

---
+++ src/calc/filter/bessel.h
@@ -0,0 +1,22 @@
+/////////////////////////////////////////////////////////////////////////////
+// Name:            bessel.cpp
+// Purpose:         Definition for the class Filter to provide functionality
+//                  for filtering data
+// Author:          Vincent Nguyen
+// Maintainer:      Vincent Nguyen
+// Contributors:    N/A
+// Created:         25/10/2023
+// Last edit:       06/10/2023
+// Copyright:       Anyone
+// Licence:         GPL-3.0 license
+/////////////////////////////////////////////////////////////////////////////
+
+namespace Calc
+{
+    namespace Filter
+    {
+        class Bessel {
+
+        };
+    }
+}
